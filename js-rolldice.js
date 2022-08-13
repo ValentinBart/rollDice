f72326ac6ad95d512ddf747e6a39fbee06516d71
Valentin Bart <valentin@bartrieul.fr>
2022-08-13 18:11:50 +0200

    Ajout d'une animation de dé

--- js-rolldice.js
+++ js-rolldice.js
@@ -1,3 +1,5 @@
+
+//fonction qui tire un nombre entre 1 et 6 au hasard et qui retour le résultat et l'image du dé
 function roll(){
     
     
@@ -6,6 +8,15 @@ function roll(){
     let retour = document.getElementById('retour');
     retour.innerText = "résultat du dé " + result;
     
+    animationDe(result);
+        
+    
+
+}
+
+//fonction qui permet de créer les différentes face du dé en fonction du résultat.
+function canvasDe(de){
+
     let canvasDe = document.getElementById('canvasDe');
     var ctx = canvasDe.getContext("2d");
     ctx.lineWidth="4";
@@ -21,7 +32,7 @@ function roll(){
     const milieu = 100;
     const bas = 125;
 
-    switch(result){
+    switch(de){
         case 1: 
         ctx.fillStyle ="#E90000";
         ctx.beginPath();
@@ -113,4 +124,40 @@ function roll(){
     }
 
 
+    
+}
+
+function aleaDe(){
+    
+    var alea = Math.floor(Math.random() * 6) + 1;
+    canvasDe(alea);
+    
+}
+
+
+
+function animationDe(int){
+   
+    setTimeout(aleaDe,1);
+    setTimeout(aleaDe,20);
+    setTimeout(aleaDe,30);
+    setTimeout(aleaDe,40);
+    setTimeout(aleaDe,50);
+    setTimeout(aleaDe,60);
+    setTimeout(aleaDe,70);
+    setTimeout(aleaDe,80);
+    setTimeout(aleaDe,100);
+    setTimeout(aleaDe,150);
+    setTimeout(aleaDe,250);
+    setTimeout(aleaDe,350);
+    setTimeout(aleaDe,450);
+    setTimeout(aleaDe,600);
+    setTimeout(aleaDe,800);
+    setTimeout(aleaDe,1000);
+    setTimeout(aleaDe,1200);
+
+    
+    setTimeout(() => {canvasDe(int)},2500);
+    
+    
 }
