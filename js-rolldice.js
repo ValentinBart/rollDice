4dc147cd838fca6737a190b6bea42faadbbbb157
Valentin Bart <valentin@bartrieul.fr>
2022-08-13 16:07:19 +0200

    Ajout du canvas pour dessiner les dé et mis en place du dé 1

--- js-rolldice.js
+++ js-rolldice.js
@@ -1,6 +1,27 @@
 function roll(){
     var result = Math.floor(Math.random() * 6) + 1;
     let retour = document.getElementById('retour');
-    retour.innerHTML = "résultat du dé " + result;
+    retour.innerText = "résultat du dé " + result;
+    
+    let canvasDe = document.getElementById('canvasDe');
+    var ctx = canvasDe.getContext("2d");
+    ctx.lineWidth="4";
+    ctx.strokeStyle="#000000";
+    ctx.rect(50,50,100,100);
+    ctx.stroke();
+
+    const radius = 6;
+
+
+    switch(result){
+        case 1: 
+        ctx.fillStyle ="#E90000";
+        ctx.beginPath();
+        ctx.arc(97,97,radius,2*Math.PI,false);
+        ctx.fill();
+        break;
+        
+    }
+
 
 }
